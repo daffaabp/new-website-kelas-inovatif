217b21247404f87c720bb79ce206bf50934943cf
bayy420-999 <getkilla.5@gmail.com>
2026-02-12 21:45:30 +0700

    Quick fix, resolve testimonials thumbnail issue in community section

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'ui-avatars.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
     ],
   },
 };
